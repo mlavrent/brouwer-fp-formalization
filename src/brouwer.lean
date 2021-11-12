92dac428d0ced8c77df86e68d95e60b019a4a7c3
Mark Lavrentyev <lavrema@outlook.com>
2021-11-12 15:50:35 -0500

    Add assumption for brouwer_func

--- src/brouwer.lean
+++ src/brouwer.lean
@@ -10,14 +10,15 @@ import .retraction
 -- lemma π₁_of_S₁_iso_ℤ : fundamental_groupoid disk 0 := sorry
 -- lemma π₁_of_D₂_iso_0 : fundamental_groupoid unit_circle ≅ ℤ := sorry
 
-def brouwer_func (p a : disk) (hp_neq_a : p ≠ a) : frontier disk :=
+def brouwer_func (f : disk → disk) (h_no_fix : ∀x, f x ≠ x) (a : disk) : frontier disk :=
 sorry
 
-lemma brouwer_func_id_on_frontier (p : disk) (a : frontier disk) (hp_neq_a : p ≠ a) :
-  true := sorry
+lemma brouwer_func_continuous (f : disk → disk) (h_no_fix : ∀x, f x ≠ x) :
+  continuous (brouwer_func f h_no_fix) :=
+sorry
 
-lemma brouwer_func_continuous :
-  continuous brouwer_func :=
+lemma brouwer_func_res_eq_id (f : disk → disk) (h_no_fix : ∀x, f x ≠ x) :
+  ∀(x : frontier disk), brouwer_func f h_no_fix ↑x = x :=
 sorry
 
 
@@ -25,18 +26,22 @@ theorem brouwer_fixed_point (f : disk → disk) :
   ∃x, f x = x :=
 begin
   by_contradiction,
-  have hno_fix : ∀x, f x ≠ x := begin
+  have h_no_fix : ∀x, f x ≠ x := begin
     rw ← not_exists,
     assumption,
    end,
 
   let r : disk → frontier disk :=
-    sorry,
+    brouwer_func f h_no_fix,
 
   have hf_retract : retraction r := {
-    retract_continuous := sorry,
+    retract_continuous := brouwer_func_continuous f h_no_fix,
     hy_sub_x := frontier_disk_subset_disk,
-    retract_of_inclusion_id := sorry,
+    retract_of_inclusion_id := begin
+      ext; rw [id],
+      { sorry, },
+      { sorry, },
+    end,
   },
 
   have hf_not_retract : ¬ retraction r := no_retraction_theorem r,
