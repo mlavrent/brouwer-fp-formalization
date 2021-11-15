f96c03cdfa06441031b0528cf954f806b67a94bc
Mark Lavrentyev <lavrema@outlook.com>
2021-11-15 09:43:08 -0500

    Pull out pi1 stuff into seperate file

---
+++ src/fundamental_group.lean
@@ -0,0 +1,27 @@
+import topology.basic
+import topology.metric_space.basic
+import topology.continuous_function.basic
+import topology.homotopy.fundamental_groupoid
+import category_theory.endomorphism
+import category_theory.groupoid
+import algebra.category.Group.basic
+import category_theory.category.basic
+
+def fundamental_group (X : Type) [topological_space X] (p : X) : Type :=
+@category_theory.Aut
+  X
+  (@category_theory.groupoid.to_category (fundamental_groupoid X) _)
+  p
+
+noncomputable instance fundamental_group.group {X : Type} [topological_space X] {p : X} : group (fundamental_group X p) :=
+@category_theory.Aut.group
+  X
+  (@category_theory.groupoid.to_category (fundamental_groupoid X) _)
+  p
+
+noncomputable def induced_hom {X Y : Type} [topological_space X] [topological_space Y] {x : X} {y : Y} (f : continuous_map X Y) (hpointed : f x = y) :
+  monoid_hom (fundamental_group X x) (fundamental_group Y y) := {
+  to_fun := λ(a : fundamental_group X x), sorry,
+  map_one' := sorry,
+  map_mul' := sorry,
+}
--- src/retraction.lean
+++ src/retraction.lean
@@ -1,20 +1,61 @@
 import topology.basic
 import topology.metric_space.basic
 import topology.continuous_function.basic
+import topology.homotopy.fundamental_groupoid
+import category_theory.endomorphism
+import category_theory.groupoid
+import algebra.category.Group.basic
+import category_theory.category.basic
 
 import .disk
+import .fundamental_group
 
-structure retraction {α : Type} [topological_space α] {X Y : set α } (r : X → Y) : Prop :=
+structure retract {α : Type} [topological_space α] {X Y : set α } (r : X → Y) : Prop :=
 (retract_continuous : continuous r)
 (hy_sub_x : Y ⊆ X)
 (retract_of_inclusion_id : r ∘ (set.inclusion hy_sub_x) = id)
 
+#check category_theory.Aut ℝ
 
--- lemma π₁_of_S₁_iso_ℤ : fundamental_groupoid disk 0 := sorry
--- lemma π₁_of_D₂_iso_0 : fundamental_groupoid unit_circle ≅ ℤ := sorry
+def π₁_S₁ := fundamental_group circle ptS₁
+def π₁_D₂ := fundamental_group disk ptD₂
+
+lemma π₁_S₁_iso_ℤ : fundamental_group circle ptS₁ ≅ ℤ := sorry
+lemma π₁_D₂_iso_0 : fundamental_group disk ptD₂ ≅ unit := sorry
+
+instance unit.has_zero : has_zero unit := {
+  zero := unit.star,
+}
+
+instance unit.add_group : add_group unit := {
+  add := λ_ _, 0,
+  zero := 0,
+  neg := λ_, 0,
+  add_assoc := by cc,
+  add_zero := by cc,
+  zero_add := by cc,
+  add_left_neg := by cc,
+}
+
+
+lemma no_surj_hom_0toℤ (φ : unit →+ ℤ) :
+  ¬ function.surjective ⇑φ :=
+begin
+  simp [function.surjective],
+  apply exists.intro (1 : ℤ),
+  intros u heq,
+  cases u,
+  sorry,
+end
+
+lemma no_surj_hom_π₁D₂_to_π₁S₁ (φ : π₁_D₂ →* π₁_S₁) :
+  ¬ function.surjective ⇑φ :=
+begin
+  sorry,
+end
 
 theorem no_retraction_theorem (f : disk → frontier disk):
-  ¬ retraction f :=
+  ¬ retract f :=
 begin
   by_contradiction,
   sorry,
