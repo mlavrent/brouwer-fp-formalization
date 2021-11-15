ceb86f0ea7e24ecffac3ea68cb5679178586c48d
Mark Lavrentyev <lavrema@outlook.com>
2021-11-15 09:42:58 -0500

    Clean up with renaming

--- src/brouwer.lean
+++ src/brouwer.lean
@@ -9,16 +9,20 @@ import .retraction
 def brouwer_func (f : disk → disk) (h_no_fix : ∀x, f x ≠ x) (a : disk) : frontier disk :=
 sorry
 
-lemma brouwer_func_continuous (f : disk → disk) (h_no_fix : ∀x, f x ≠ x) :
+lemma brouwer_func_continuous (f : disk → disk) (hf_cont : continuous f) (h_no_fix : ∀x, f x ≠ x) :
   continuous (brouwer_func f h_no_fix) :=
 sorry
 
-lemma brouwer_func_res_eq_id (f : disk → disk) (h_no_fix : ∀x, f x ≠ x) :
+lemma brouwer_func_circle_eq_id (f : disk → disk) (hf_cont : continuous f) (h_no_fix : ∀x, f x ≠ x) :
   ∀(x : frontier disk), brouwer_func f h_no_fix ↑x = x :=
-sorry
+begin
+  intro x,
+  simp [brouwer_func],
+  sorry,
+end
 
 
-theorem brouwer_fixed_point (f : disk → disk) :
+theorem brouwer_fixed_point (f : disk → disk) (hf_cont : continuous f) :
   ∃x, f x = x :=
 begin
   by_contradiction,
@@ -30,17 +34,17 @@ begin
   let r : disk → frontier disk :=
     brouwer_func f h_no_fix,
 
-  have hf_retract : retraction r := {
-    retract_continuous := brouwer_func_continuous f h_no_fix,
+  have hf_retract : retract r := {
+    retract_continuous := brouwer_func_continuous f hf_cont h_no_fix,
     hy_sub_x := frontier_disk_subset_disk,
     retract_of_inclusion_id := begin
       apply funext,
       intro x,
       simp [r],
-      apply brouwer_func_res_eq_id f h_no_fix,
+      apply brouwer_func_circle_eq_id f hf_cont h_no_fix,
     end,
   },
 
-  have hf_not_retract : ¬ retraction r := no_retraction_theorem r,
+  have hf_not_retract : ¬ retract r := no_retraction_theorem r,
   contradiction,
 end
