ebeea6f4c00ae97f3d736bf2f0e37c2f6f6fb5f3
Mark Lavrentyev <lavrema@outlook.com>
2021-11-21 11:13:37 -0500

    Add pointed space stuff

---
+++ src/pointed_space.lean
@@ -0,0 +1,11 @@
+import topology.basic
+import topology.continuous_function.basic
+
+@[class]
+structure pointed_space (α : Type) extends topological_space α : Type :=
+(basepoint : α)
+
+structure pointed_continuous_map (α β : Type)
+  [pointed_space α] [pointed_space β]
+  extends continuous_map α β : Type :=
+(pointed_map : to_fun (@pointed_space.basepoint α _) = @pointed_space.basepoint β _)
