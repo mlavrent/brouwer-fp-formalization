0433be102652d9b57f6e2e0f8fec36bc96208cdf
Mark Lavrentyev <lavrema@outlook.com>
2021-11-28 08:04:24 -0500

    Add way to construct pointed map from continuous map

--- src/pointed_space.lean
+++ src/pointed_space.lean
@@ -2,7 +2,7 @@ import topology.basic
 import topology.continuous_function.basic
 
 structure pointed_space (X : Type) [topological_space X] : Type :=
-(to_topological_space : topological_space X)
+-- (to_topological_space : topological_space X)
 (basepoint : X)
 
 structure pointed_continuous_map {X Y : Type} [topological_space X] [topological_space Y]
@@ -16,3 +16,9 @@ instance has_coe_to_fun.pointed_continuous_map {X Y : Type} [topological_space X
   (Xp : pointed_space X) (Yp : pointed_space Y) :
   has_coe_to_fun Cp(Xp, Yp) (λ _, X → Y) :=
 ⟨λf, pointed_continuous_map.to_continuous_map f⟩
+
+def pointed_map_of_continuous_map {X Y : Type} [topological_space X] [topological_space Y] [inhabited X]
+  (f : C(X, Y)) : Cp((pointed_space.mk (inhabited.default X)), (pointed_space.mk (f (inhabited.default X)))) := {
+  to_fun := f,
+  pointed_map := by simp,
+}
